ff5a4a1a1fd0773cb22d38f325aa9da707dfa10c
DenisBkrr <161237470+DenisBkrr@users.noreply.github.com>
2024-03-17 16:58:25 +0200

    Add files via upload

---
+++ ProjectOOP/src/Project/Course.java
@@ -0,0 +1,11 @@
+package Project;
+
+public class Course {
+    public String name;
+    public String type;
+
+    public Course(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+}
---
+++ ProjectOOP/src/Project/SubjectsCourse.java
@@ -0,0 +1,11 @@
+package Project;
+
+public class SubjectsCourse {
+    public String specialty;
+    public Course course;
+
+    public SubjectsCourse(String specialty, Course course) {
+        this.specialty = specialty;
+        this.course = course;
+    }
+}
